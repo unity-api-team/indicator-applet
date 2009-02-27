9c8a8b5c99516c5a8a6b8f287ffe2a874e5a7082
Mark Lee <bzr@lazymalevolence.com>
2009-02-27 00:32:13 -0800

    Fix undefined reference to GTK_MENU_ITEM.

--- src/applet-main.c
+++ src/applet-main.c
@@ -20,6 +20,7 @@ You should have received a copy of the GNU General Public License along
 with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#include <gtk/gtk.h>
 #include <panel-applet.h>
 
 #define SYMBOL_NAME  "get_menu_item"
