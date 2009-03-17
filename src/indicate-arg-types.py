a8be54219a8e5aad94361095027a6a561d092b38
Eitan Isaacson <eitan@ascender.com>
2009-03-17 12:10:28 -0700

    Cleanup. Now compiles with no warnings (but need to override two more functions).

--- src/indicate-arg-types.py
+++ src/indicate-arg-types.py
@@ -4,8 +4,6 @@ argtypes.matcher.register('IndicateListenerServer', argtypes.CharArg())
 
 argtypes.matcher.register('IndicateListenerServer*', argtypes.StringArg())
  
-argtypes.matcher.register('IndicateListenerIndicator', argtypes.IntArg())
+argtypes.matcher.register('IndicateListenerIndicator', argtypes.UIntArg())
 
-# !!!
-argtypes.matcher.register('IndicateListenerIndicator*', argtypes.IntArg())
 
