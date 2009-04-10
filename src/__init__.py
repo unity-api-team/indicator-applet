d3f623b624b4c1fdf3d3b6f1b4706d6452f179fe
Eitan Isaacson <eitan@ascender.com>
2009-04-10 18:18:45 +0300

    Removed obsolete gpointer hacks from __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -24,16 +24,3 @@
 # <http://www.gnu.org/licenses/>
 
 from _indicate import *
-import ctypes
-
-def server_dbus_name(gpointer):
-    p = int(str(gpointer)[13:-1], 16)
-    try:
-        rv = ctypes.cast(p,ctypes.c_char_p).value
-    except ctypes.ArgumentError:
-        return None
-    return rv
-
-def indicator_id(gpointer):
-    return int(str(gpointer)[13:-1],16)
-
