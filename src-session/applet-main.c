7957484b0319c60d4b1bbb7e6055f35b39f0a631
Martin Pitt <martin.pitt@canonical.com>
2009-10-14 20:16:48 +0200

    Hide session applet in stracciatella session.
    This is the same code as for the message indicator applet.

--- src-session/applet-main.c
+++ src-session/applet-main.c
@@ -214,6 +214,12 @@ applet_fill_cb (PanelApplet * applet, const gchar * iid, gpointer data)
 	gint indicators_loaded = 0;
 	static gboolean first_time = FALSE;
 
+	/* check if we are running stracciatella session */
+	if (g_strcmp0(g_getenv("GDMSESSION"), "gnome-stracciatella") == 0) {
+		g_debug("Running stracciatella GNOME session, disabling myself");
+		return TRUE;
+	}
+
 	if (!first_time)
 	{
         gint argc = 1;
