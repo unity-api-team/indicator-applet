419bac217321b2b366e63858e2f55996d2d4636c
Mitch Towner <mitch.towner.ubuntu@gmail.com>
2010-06-15 13:18:30 +1000

    * debian/source_indicator-applet.py - Add apport hook. (LP: #583174)

---
+++ debian/source_indicator-applet.py
@@ -0,0 +1,40 @@
+import os.path
+from apport.hookutils import *
+from xdg.BaseDirectory import xdg_cache_home
+
+APPLET_LOG = os.path.join(xdg_cache_home, 'indicator-applet.log')
+APPLET_SESSION_LOG = os.path.join(xdg_cache_home, 'indicator-applet-session.log')
+COMPLETE_LOG = os.path.join(xdg_cache_home, 'indicator-applet-complete.log')
+APPMENU_LOG = os.path.join(xdg_cache_home, 'indicator-applet-appmenu.log')
+
+def add_info(report, ui):
+
+    response = ui.choice("How would you describe the issue?", ["None of the indicator applets are working correctly", "The sound applet is not working correctly", "The Social Networking Menu is not working correctly", "The login/logout applet is not working correctly", "The network applet is not working correctly", "The date/time applet in not working correctly", "The application testing applet in not working correctly", "The all-in-one (complete) applet is not working correctly", "Another applet is not working correctly"], False)
+    try:
+        if response == None: # user cancelled
+            raise StopIteration
+        if response[0] == 0: # an issue with indicator-applet
+            report.add_package_info("indicator-applet")
+        if response[0] == 1: # the issue is with indicator-sound
+            report.add_package_info("indicator-sound")
+        if response[0] == 2: # the issue is with indicator-me
+            report.add_package_info("indicator-me")
+        if response[0] == 3: # the issue is with indicator-session
+            report.add_package_info("indicator-session")
+        if response[0] == 4: # the issue is with indicator-network in UNE
+            report.add_package_info("indicator-network")
+        if response[0] == 5: # the issue is with indicator-datetime in "unity"
+            report.add_package_info("indicator-datetime")
+        if response[0] == 6: # the issue is with indicator-applet-appmenu
+            report.add_package_info("indicator-applet-appmenu")
+        if response[0] == 7: # the issue is with indicator-applet-complete
+            report.add_package_info("indicator-applet-complete")            
+        if response[0] == 8: # the issue is with indicator-application
+            report.add_package_info("indicator-application")
+    except:
+        print 'Unfortunately, that package is not installed. Please refer to https://wiki.ubuntu.com/Bugs/FindRightPackage'
+
+    attach_file_if_exists(report, APPLET_LOG, 'indicator-applet.log')
+    attach_file_if_exists(report, APPLET_SESSION_LOG, 'applet-session.log')
+    attach_file_if_exists(report, COMPLETE_LOG, 'indicator-applet-complete.log')
+    attach_file_if_exists(report, APPMENU_LOG, 'indicator-applet-appmenu.log')
