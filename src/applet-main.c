d84e9020d703bfe8faff3c493a049605aeb6c5b4
Alberts Muktupavels <alberts.muktupavels@gmail.com>
2013-07-10 15:08:30 +0300

    Load indicator objects from keyfiles in /usr/share/unity/indicators. (LP: #1197957)

--- src/applet-main.c
+++ src/applet-main.c
@@ -27,7 +27,8 @@ with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <panel-applet.h>
 #include <gdk/gdkkeysyms.h>
 
-#include "libindicator/indicator-object.h"
+#include <libindicator/indicator-object.h>
+#include <libindicator/indicator-ng.h>
 #include "tomboykeybinder.h"
 
 static const gchar * indicator_order[][2] = {
@@ -605,12 +606,50 @@ update_accessible_desc(IndicatorObjectEntry * entry, GtkWidget * menuitem)
   return;
 }
 
+static void load_indicator(GtkWidget * menubar, IndicatorObject *object, const gchar *_name) {
+	GObject * o;
+	gchar *name;
+	GList *entries, *entry;
+
+	/* Set the environment it's in */
+	indicator_object_set_environment(object, (GStrv)indicator_env);
+
+	/* */
+	if (_name != NULL) {
+		name = g_strdup (_name);
+	}else{
+		name = g_strdup_printf ("%p", object);
+	}
+
+	/* Attach the 'name' to the object */
+	o = G_OBJECT (object);
+	g_object_set_data_full(o, IO_DATA_MENUITEM_LOOKUP, g_hash_table_new (g_direct_hash, g_direct_equal), (GDestroyNotify)g_hash_table_destroy);
+	g_object_set_data_full(o, IO_DATA_NAME, g_strdup(name), g_free);
+	g_object_set_data(o, IO_DATA_ORDER_NUMBER, GINT_TO_POINTER(name2order(name, NULL)));
+
+	/* Connect to its signals */
+	g_signal_connect(o, INDICATOR_OBJECT_SIGNAL_ENTRY_ADDED,   G_CALLBACK(entry_added),    menubar);
+	g_signal_connect(o, INDICATOR_OBJECT_SIGNAL_ENTRY_REMOVED, G_CALLBACK(entry_removed),  menubar);
+	g_signal_connect(o, INDICATOR_OBJECT_SIGNAL_ENTRY_MOVED,   G_CALLBACK(entry_moved),    menubar);
+	g_signal_connect(o, INDICATOR_OBJECT_SIGNAL_MENU_SHOW,     G_CALLBACK(menu_show),      menubar);
+	g_signal_connect(o, INDICATOR_OBJECT_SIGNAL_ACCESSIBLE_DESC_UPDATE, G_CALLBACK(accessible_desc_update), menubar);
+
+	/* Work on the entries */
+	entries = indicator_object_get_entries(object);
+
+	for (entry = entries; entry != NULL; entry = g_list_next(entry)) {
+		IndicatorObjectEntry * entrydata = (IndicatorObjectEntry *) entry->data;
+		entry_added(object, entrydata, menubar);
+	}
+
+	g_list_free(entries);
+	g_free (name);
+}
 
 static gboolean
 load_module (const gchar * name, GtkWidget * menubar)
 {
-  GObject * o;
-
+  
   g_debug("Looking at Module: %s", name);
   g_return_val_if_fail(name != NULL, FALSE);
 
@@ -625,34 +664,96 @@ load_module (const gchar * name, GtkWidget * menubar)
   IndicatorObject * io = indicator_object_new_from_file(fullpath);
   g_free(fullpath);
 
-  /* Set the environment it's in */
-  indicator_object_set_environment(io, (GStrv)indicator_env);
-
-  /* Attach the 'name' to the object */
-  o = G_OBJECT (io);
-  g_object_set_data_full(o, IO_DATA_MENUITEM_LOOKUP, g_hash_table_new (g_direct_hash, g_direct_equal), (GDestroyNotify)g_hash_table_destroy);
-  g_object_set_data_full(o, IO_DATA_NAME, g_strdup(name), g_free);
-  g_object_set_data(o, IO_DATA_ORDER_NUMBER, GINT_TO_POINTER(name2order(name, NULL)));
-
-  /* Connect to its signals */
-  g_signal_connect(o, INDICATOR_OBJECT_SIGNAL_ENTRY_ADDED,   G_CALLBACK(entry_added),    menubar);
-  g_signal_connect(o, INDICATOR_OBJECT_SIGNAL_ENTRY_REMOVED, G_CALLBACK(entry_removed),  menubar);
-  g_signal_connect(o, INDICATOR_OBJECT_SIGNAL_ENTRY_MOVED,   G_CALLBACK(entry_moved),    menubar);
-  g_signal_connect(o, INDICATOR_OBJECT_SIGNAL_MENU_SHOW,     G_CALLBACK(menu_show),      menubar);
-  g_signal_connect(o, INDICATOR_OBJECT_SIGNAL_ACCESSIBLE_DESC_UPDATE, G_CALLBACK(accessible_desc_update), menubar);
-
-  /* Work on the entries */
-  GList * entries = indicator_object_get_entries(io);
-  GList * entry = NULL;
-
-  for (entry = entries; entry != NULL; entry = g_list_next(entry)) {
-    IndicatorObjectEntry * entrydata = (IndicatorObjectEntry *)entry->data;
-    entry_added(io, entrydata, menubar);
-  }
-
-  g_list_free(entries);
+  load_indicator(menubar, io, name);
 
   return TRUE;
+}
+
+static void load_modules(GtkWidget *menubar, gint *indicators_loaded) {
+	if  (g_file_test(INDICATOR_DIR, (G_FILE_TEST_EXISTS | G_FILE_TEST_IS_DIR))) {
+		GDir * dir = g_dir_open(INDICATOR_DIR, 0, NULL);
+
+		const gchar * name;
+		gint count = 0;
+		while ((name = g_dir_read_name(dir)) != NULL) {
+			
+			#ifdef INDICATOR_APPLET_APPMENU
+				if (g_strcmp0(name, "libappmenu.so")) {
+					continue;
+				}
+			#else
+				if (!g_strcmp0(name, "libappmenu.so")) {
+					continue;
+				}
+			#endif
+			
+			#ifdef INDICATOR_APPLET
+				if (!g_strcmp0(name, "libsession.so")) {
+					continue;
+				}
+				if (!g_strcmp0(name, "libme.so")) {
+					continue;
+				}
+				if (!g_strcmp0(name, "libdatetime.so")) {
+					continue;
+				}
+			#endif
+			
+			#ifdef INDICATOR_APPLET_SESSION
+				if (g_strcmp0(name, "libsession.so") && g_strcmp0(name, "libme.so")) {
+					continue;
+				}
+			#endif
+
+			if (load_module(name, menubar)) {
+				count++;
+			}
+		}
+
+		*indicators_loaded = count;
+		
+		g_dir_close (dir);
+	}
+}
+
+#define INDICATOR_SERVICE_DIR "/usr/share/unity/indicators"
+
+static void load_indicators_from_indicator_files(GtkWidget *menubar, gint *indicators_loaded) {
+	GDir *dir;
+	const gchar *name;
+	GError *error = NULL;
+
+	dir = g_dir_open (INDICATOR_SERVICE_DIR, 0, &error);
+
+	if (!dir) {
+		g_warning ("unable to open indicator service file directory: %s", error->message);
+  		g_error_free (error);
+		
+  		return;
+	}
+	
+	gint count = 0;
+	while ((name = g_dir_read_name (dir))) {
+		gchar *filename;
+		IndicatorNg *indicator;
+
+		filename = g_build_filename (INDICATOR_SERVICE_DIR, name, NULL);
+		indicator = indicator_ng_new_for_profile (filename, "desktop", &error);
+
+		if (indicator) {
+			load_indicator(menubar,INDICATOR_OBJECT (indicator), NULL);
+			count++;
+		}else{
+			g_warning ("unable to load '%s': %s", name, error->message);
+			g_clear_error (&error);
+		}
+
+		g_free (filename);
+	}
+
+	*indicators_loaded = count;
+
+	g_dir_close (dir);
 }
 
 static void
@@ -957,43 +1058,9 @@ applet_fill_cb (PanelApplet * applet, const gchar * iid G_GNUC_UNUSED,
   /* Add in filter func */
   tomboy_keybinder_bind(hotkey_keycode, hotkey_filter, menubar);
 
-  /* load 'em */
-  if (g_file_test(INDICATOR_DIR, (G_FILE_TEST_EXISTS | G_FILE_TEST_IS_DIR))) {
-    GDir * dir = g_dir_open(INDICATOR_DIR, 0, NULL);
-
-    const gchar * name;
-    while ((name = g_dir_read_name(dir)) != NULL) {
-#ifdef INDICATOR_APPLET_APPMENU
-      if (g_strcmp0(name, "libappmenu.so")) {
-        continue;
-      }
-#else
-      if (!g_strcmp0(name, "libappmenu.so")) {
-        continue;
-      }
-#endif
-#ifdef INDICATOR_APPLET
-      if (!g_strcmp0(name, "libsession.so")) {
-        continue;
-      }
-      if (!g_strcmp0(name, "libme.so")) {
-        continue;
-      }
-      if (!g_strcmp0(name, "libdatetime.so")) {
-        continue;
-      }
-#endif
-#ifdef INDICATOR_APPLET_SESSION
-      if (g_strcmp0(name, "libsession.so") && g_strcmp0(name, "libme.so")) {
-        continue;
-      }
-#endif
-      if (load_module(name, menubar)) {
-        indicators_loaded++;
-      }
-    }
-    g_dir_close (dir);
-  }
+	/* load indicators */
+	load_modules(menubar, &indicators_loaded);
+	load_indicators_from_indicator_files(menubar, &indicators_loaded);
 
   if (indicators_loaded == 0) {
     /* A label to allow for click through */
@@ -1010,4 +1077,3 @@ applet_fill_cb (PanelApplet * applet, const gchar * iid G_GNUC_UNUSED,
 
   return TRUE;
 }
-
